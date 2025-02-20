6965e22bb075b59cffe49324e733d8fe80f173df
Krozs1607 <igorferreira@ufba.br>
2025-02-20 11:48:24 -0300

    app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ def generate_combinations_with_openai(prompt):
     """Usa a API da OpenAI para gerar combinações de lanches."""
     try:
         resposta = openai.chat.completions.create(
-            model="gpt-4",
+            model="pgt-4o-mini",
             messages=[
                 {
                     "role": "system",
